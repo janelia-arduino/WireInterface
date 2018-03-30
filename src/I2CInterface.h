4af92e357453e4d795fef9f7b344160ec7f88427
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:11:38 -0400

    Removed unnecessary header includes.

--- src/I2CInterface.h
+++ src/I2CInterface.h
@@ -7,23 +7,12 @@
 // ----------------------------------------------------------------------------
 #ifndef I2C_INTERFACE_H
 #define I2C_INTERFACE_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
-#include <ModularClient.h>
 
 #include <ModularServer.h>
 #include <ModularDeviceBase.h>
